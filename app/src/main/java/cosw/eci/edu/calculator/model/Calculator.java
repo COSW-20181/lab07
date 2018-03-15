d1d02cff57df37d48e524e2c1c67d25692252d1d
Hernan Felipe Losada <hernan.losada96@gmail.com>
2018-03-15 10:57:44 -0500

    end lab

---
+++ app/src/main/java/cosw/eci/edu/calculator/model/Calculator.java
@@ -0,0 +1,50 @@
+package cosw.eci.edu.calculator.model;
+
+import java.util.Stack;
+
+/**
+ * Created by 2105409 on 3/15/18.
+ */
+
+public class Calculator {
+
+    public double calculate(Stack<String> tks) throws Exception {
+        return calculateInternal(tks);
+    }
+
+
+    private double calculateInternal(Stack<String> tks) throws Exception {
+        String tk = tks.pop();
+        double x, y;
+        try {
+            x = Double.parseDouble(tk);
+        } catch (Exception e) {
+            y = calculateInternal(tks);
+            x = calculateInternal(tks);
+            if (tk.equals("+")) x += y;
+            else if (tk.equals("-")) x -= y;
+            else if (tk.equals("*")) x *= y;
+            else if (tk.equals("/")) x /= y;
+            else throw new Exception();
+        }
+        return x;
+    }
+
+    public Boolean isNumberOrOperator(String toEval) {
+        Boolean ans = true;
+        try {
+            Double x = Double.parseDouble(toEval);
+        } catch (Exception e) {
+            ans = (toEval.equals("+") || toEval.equals("-") || toEval.equals("*") || toEval.equals("/"));
+        }
+        return ans;
+    }
+
+    public String tryChangeNumber(String num) throws Exception {
+        String ans = num;
+        Double x = Double.parseDouble(num);
+        if (x>0) ans="-"+ans;
+        else if (x<0) ans=ans.substring(1);
+        return ans;
+    }
+}
